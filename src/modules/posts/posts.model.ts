ccbcfdc14ae801edbfe0ea33e702a86d05a45f79
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2026-06-06 17:46:28 +0300

    Устанновка пакетов для joi npm install --save @nestjs/config joi добавлен config/validation.ts добавлен config/configuration.ts пакеты типы Node.js npm install --save-dev @types/node конфиг добавлен в app.module.ts

--- src/modules/posts/posts.model.ts
+++ src/modules/posts/posts.model.ts
@@ -2,8 +2,8 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Post {
-    @PrimaryGeneratedColumn('uuid')
-    id!: string;
+    @PrimaryGeneratedColumn()
+    id!: number;  //теперь number
 
     @Column()
     title!: string;
